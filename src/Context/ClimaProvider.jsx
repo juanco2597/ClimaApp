1a2650ebb7f747280aea5af77d7b88c7bb24328a
Juan <juan.parra@smartsoft.com.co>
2023-03-12 12:56:38 -0500

    Modificaciones y creaciones de componentes

---
+++ src/Context/ClimaProvider.jsx
@@ -0,0 +1,36 @@
+import { createContext, useState } from 'react'
+
+const ClimaContext = createContext()
+const ClimaProvider = ({ children }) => {
+
+    const [busqueda, setBusqueda] = useState({
+        ciudad: '',
+        pais: ''
+    })
+
+    const datosBusqueda = () => {
+        setBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const consultarClima = datos => {
+        console.log(datos)
+    }
+    return (
+        <ClimaContext.Provider
+            value={{
+                busqueda,
+                datosBusqueda
+            }}
+        >
+            {children}
+        </ClimaContext.Provider>
+    )
+}
+
+export {
+    ClimaProvider
+}
+export default ClimaContext
